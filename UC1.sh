e346ebd61f91e0bf3d6050f67e4a0015d0315f8e
swapnil <sopiswarrinku77@gmail.com>
2022-04-12 17:27:20 -0700

    employees attendence

---
+++ UC1.sh
@@ -0,0 +1,20 @@
+read -p "Enter a number for attendence 1 and 2 :" num
+
+if [ $num == 1 ];
+
+then
+
+echo " employee is present today.. "
+
+elif [ $num == 2 ];
+
+then
+
+echo "employee is absend today.."
+
+fi
+
+
+
+
+
